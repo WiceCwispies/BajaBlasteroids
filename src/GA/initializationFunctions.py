31f273acd3bdf688f8edd2511116ffd26c8533e3
Daniel Heitmeyer <heitmedl@mail.uc.edu>
2022-10-17 19:19:12 -0400

    Added old GA and Fuzzy files after cleaning

---
+++ src/GA/initializationFunctions.py
@@ -0,0 +1,13 @@
+import numpy as np
+import random
+from GA.chromosome import *
+
+
+def asteriodInitialize(numbChroms):
+    population = []
+    for i in range(numbChroms):
+        population.append(Chromosome(np.random.randint(3, size=(2, 54)).tolist()))
+    return population
+
+if __name__ == "__main__":
+    asteriodInitialize(10)
