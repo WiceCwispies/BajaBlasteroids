a617227f01d6f20b9a100f71c2b9f452f445fbb6
Garrison <wettengl@mail.uc.edu>
2022-12-05 20:54:04 -0500

    Do not run this commit

--- src/controller.py
+++ src/controller.py
@@ -1,7 +1,7 @@
 from typing import Tuple, Dict, Any, List
 
 from fuzzy_asteroids.fuzzy_controller import ControllerBase, SpaceShip
-
+from src.vector_utils.trajectories import *
 
 class FuzzyController(ControllerBase):
     """
@@ -53,7 +53,7 @@ class FuzzyController(ControllerBase):
         stopping_condition = input_data['stopping_condition']
         # print(stopping_condition)
         asteroids = input_data['asteroids']
-        # print(asteroids)
+        print(asteroids)
         map_dimensions = input_data['map_dimensions']
         # print(map_dimensions)
         bullets = input_data['bullets']
@@ -64,15 +64,36 @@ class FuzzyController(ControllerBase):
         # BROKEN CURRENTLY 
         # ship_position = ships_info(0)['position']
 
+        # ships_info[]
+
+        # # AIMING
+
+        ship_x = ships_info[0]['position'][0]
+        ship_y = ships_info[0]['position'][1]
+        ship_position = [ship_x, ship_y]
+        speed = 800
+        asteroid_x = asteroids[0]['position'][0]
+        asteroid_y = asteroids[0]['position'][1]
+        asteroid_position = [asteroid_x, asteroid_y]
+        asteroid_x_speed = asteroids[0]['velocity'][0]
+        asteroid_y_speed = asteroids[0]['velocity'][1]
+        asteroid_velocity = [asteroid_x_speed, asteroid_y_speed]
+        firing_angle = find_desired_angle(ship_position, speed, asteroid_position, asteroid_velocity)
+
+        while abs(firing_angle) - abs(ships_info[0]['angle']) > 2:
+            ships.turn_rate = 20
+
+
+        ships.shoot()
         # for ship in ships:
         #ships.turn_rate = 23
         #ships.thrust = ships.thrust_range[0.5]
         #print("x")
         # print(asteroids)
         #print(input_data['asteroids'][0]['position'][0])
-        if abs(ships.position[0] - asteroids[0]['position'][0]) <= 150:
-            ships.shoot()
-            print("he")
+        # if abs(ships.position[0] - asteroids[0]['position'][0]) <= 150:
+        #     ships.shoot()
+
         
 
 
